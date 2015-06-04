d01819962086a185c56e4875b3b4c7c5725fa030
Michael Twomey <michael.twomey@udemy.com>
2015-06-04 10:56:50 -0700

    Bumping to version 2

--- acaversity/views.py
+++ acaversity/views.py
@@ -5,7 +5,7 @@ from django.http import (
 
 
 def index(request):
-    version = 1
+    version = 2
     html = "<html><body><h1>Hello</h1><p>What do you want to learn today?</p><p>Version {}</p></body></html>".format(version)
     return HttpResponse(html)
 
