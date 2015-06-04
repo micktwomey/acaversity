6b30463cdc4c5621f1944a27f1574717301bff04
Michael Twomey <michael.twomey@udemy.com>
2015-06-04 11:18:19 -0700

    Using uwsgi + health port

--- slug/scripts/start.sh
+++ slug/scripts/start.sh
@@ -3,4 +3,8 @@
 . $HOME/venv/bin/activate
 
 echo "Starting with args $@"
-exec ./venv/bin/python ./venv/bin/django-admin runserver --settings=acaversity.settings "$@"
+# exec ./venv/bin/python ./venv/bin/django-admin runserver --settings=acaversity.settings "$@"
+
+exec ./venv/bin/uwsgi --home $HOME/venv --master \
+--wsgi-file $HOME/venv/lib/python2.7/site-packages/acaversity/wsgi.py \
+"$@"
